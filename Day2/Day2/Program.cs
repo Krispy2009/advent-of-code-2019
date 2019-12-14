81e936c4ca67890a3efb7d57c8db351b9381c674
Kristi Achilleos <Krispy2009@gmail.com>
2019-12-14 15:26:33 +0200

    refactor part1 to allow for part 2

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -10,12 +10,11 @@ namespace Day2
 
     class Part1
     {
-        static void Main(string[] args)
+        public int RunProgram(int[] progInput, int startValue1, int startValue2)
         {
-            int[] progInput = { 1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 2, 6, 19, 23, 1, 23, 5, 27, 1, 27, 13, 31, 2, 6, 31, 35, 1, 5, 35, 39, 1, 39, 10, 43, 2, 6, 43, 47, 1, 47, 5, 51, 1, 51, 9, 55, 2, 55, 6, 59, 1, 59, 10, 63, 2, 63, 9, 67, 1, 67, 5, 71, 1, 71, 5, 75, 2, 75, 6, 79, 1, 5, 79, 83, 1, 10, 83, 87, 2, 13, 87, 91, 1, 10, 91, 95, 2, 13, 95, 99, 1, 99, 9, 103, 1, 5, 103, 107, 1, 107, 10, 111, 1, 111, 5, 115, 1, 115, 6, 119, 1, 119, 10, 123, 1, 123, 10, 127, 2, 127, 13, 131, 1, 13, 131, 135, 1, 135, 10, 139, 2, 139, 6, 143, 1, 143, 9, 147, 2, 147, 6, 151, 1, 5, 151, 155, 1, 9, 155, 159, 2, 159, 6, 163, 1, 163, 2, 167, 1, 10, 167, 0, 99, 2, 14, 0, 0 };
-            // Replace values as instructed
-            progInput[1] = 12;
-            progInput[2] = 2;
+            // Replace values as instructed in part 1
+            progInput[1] = startValue1;
+            progInput[2] = startValue2;
 
 
             int idx = 0;
@@ -29,19 +28,18 @@ namespace Day2
                 switch (opcode)
                 {
                     case 1:
-                        Console.WriteLine("Adding");
+                       //Console.WriteLine("Adding");
                         progInput[destination] = progInput[first_position] + progInput[second_position];
                         break;
                     case 2:
-                        Console.WriteLine("Multiplying");
+                        //Console.WriteLine("Multiplying");
 
                         progInput[destination] = progInput[first_position] * progInput[second_position];
                         break;
                     case 99:
                         Console.WriteLine("Program Halted!");
-                        Console.WriteLine(progInput[0]);
-                        Console.ReadKey();
-                        break;
+                        return progInput[0];
+
                     default:
                         Console.WriteLine("ERROR!");
                         break;
@@ -50,9 +48,24 @@ namespace Day2
 
                 idx += 4;
             }
-
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // If we haven't returned a number by now something has gone wrong
+            return -1;
         }
+
+        static void Main(string[] args)
+        {
+            int[] progInput = { 1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 2, 6, 19, 23, 1, 23, 5, 27, 1, 27, 13, 31, 2, 6, 31, 35, 1, 5, 35, 39, 1, 39, 10, 43, 2, 6, 43, 47, 1, 47, 5, 51, 1, 51, 9, 55, 2, 55, 6, 59, 1, 59, 10, 63, 2, 63, 9, 67, 1, 67, 5, 71, 1, 71, 5, 75, 2, 75, 6, 79, 1, 5, 79, 83, 1, 10, 83, 87, 2, 13, 87, 91, 1, 10, 91, 95, 2, 13, 95, 99, 1, 99, 9, 103, 1, 5, 103, 107, 1, 107, 10, 111, 1, 111, 5, 115, 1, 115, 6, 119, 1, 119, 10, 123, 1, 123, 10, 127, 2, 127, 13, 131, 1, 13, 131, 135, 1, 135, 10, 139, 2, 139, 6, 143, 1, 143, 9, 147, 2, 147, 6, 151, 1, 5, 151, 155, 1, 9, 155, 159, 2, 159, 6, 163, 1, 163, 2, 167, 1, 10, 167, 0, 99, 2, 14, 0, 0 };
+
+            Part1 part = new Part1();
+            int result = part.RunProgram(progInput, 12, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    class Part2
+    {
+        int[] progInput = { 1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 2, 6, 19, 23, 1, 23, 5, 27, 1, 27, 13, 31, 2, 6, 31, 35, 1, 5, 35, 39, 1, 39, 10, 43, 2, 6, 43, 47, 1, 47, 5, 51, 1, 51, 9, 55, 2, 55, 6, 59, 1, 59, 10, 63, 2, 63, 9, 67, 1, 67, 5, 71, 1, 71, 5, 75, 2, 75, 6, 79, 1, 5, 79, 83, 1, 10, 83, 87, 2, 13, 87, 91, 1, 10, 91, 95, 2, 13, 95, 99, 1, 99, 9, 103, 1, 5, 103, 107, 1, 107, 10, 111, 1, 111, 5, 115, 1, 115, 6, 119, 1, 119, 10, 123, 1, 123, 10, 127, 2, 127, 13, 131, 1, 13, 131, 135, 1, 135, 10, 139, 2, 139, 6, 143, 1, 143, 9, 147, 2, 147, 6, 151, 1, 5, 151, 155, 1, 9, 155, 159, 2, 159, 6, 163, 1, 163, 2, 167, 1, 10, 167, 0, 99, 2, 14, 0, 0 };
+
     }
 }
